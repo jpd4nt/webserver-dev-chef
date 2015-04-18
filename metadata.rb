7d412e58d2116ff0fa1131e12068d638aad6b67f
John-Paul Drawneek <jdrawneek@nationaltheatre.org.uk>
2015-04-18 21:48:33 +0100

    update to chef 12 and move mysql stuff

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,10 @@
+name             "webserver-dev-chef"
 maintainer       "National Theatre"
 maintainer_email "jdrawneek@nationaltheatre.org.uk"
 license          "All rights reserved"
 description      "Installs/Configures webserver-dev-chef"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.0"
+version          "0.3.0"
 depends          "webserver-chef", ">= 0.0.1"
 depends          "php",            ">= 0.0.1"
 depends          "mysql",          "< 5.0.0"
